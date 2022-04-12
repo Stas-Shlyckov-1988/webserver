ea659a2791c0cd4beffb3d9468892eb52098813e
Stas <mpeegy@ya.ru>
2022-04-12 10:03:30 +0300

    Add title

--- main.c
+++ main.c
@@ -13,6 +13,7 @@ void route()
     ROUTE_GET("/")
     {
         printf("HTTP/1.1 200 OK\r\n\r\n");
+		printf("<head><title>My web server!</title></head>");
         printf("<h1>Hello! You are using</h1> <p style=\"color: blue;\">%s</p>", request_header("User-Agent"));
 		printf("<br><a href=\"/detail\">detail</a>.");
     }
@@ -28,6 +29,7 @@ void route()
 	ROUTE_GET("/detail")
 	{
 		printf("HTTP/1.1 200 OK\r\n\r\n");
+		printf("<head><title>My web server!</title></head>");
 		printf("<h1>Example web site.</h1>");
 		printf("<p>This is detail page.<br>Current web site from web server of the fork nginx.</p>");
 	}
